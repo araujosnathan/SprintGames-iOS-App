acf8df0f4c8d3a83f165b59646c1fce314837cec
Nathanael <nathanael@cs-571.local>
2018-05-26 23:37:18 -0300

    Initial Commit

---
+++ SprintGames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SprintGames
+//
+//  Created by Nathanael on 26/05/2018.
+//  Copyright © 2018 Nathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
